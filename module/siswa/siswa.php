a208f808152d4ca5271e747b947b5180ea22131f
Alaik Haikal Alfaizi <alaikhaikal.951@gmail.com>
2024-05-26 22:36:02 +0700

    initial commit

---
+++ module/siswa/siswa.php
@@ -0,0 +1,135 @@
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header"><strong>Data Siswa</strong></h3>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <?php
+                            $klas = $_GET['kls'];
+                            if ($klas == "semua") {
+                                echo "Data Semua Siswa";
+                            } else {
+                                $claris = mysqli_query($conn, "select * from kelas where idk='$_GET[kls]'");
+                                $click = mysqli_fetch_array($claris);
+                                echo "Data Siswa Kelas $click[nama]";
+                            }
+                            ?>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">NIS</th>
+                                            <th class="text-center" width="30%">Nama</th>
+                                            <th class="text-center">JK</th>
+                                            <th class="text-center">Kelas</th>
+                                            <th class="text-center">No Telepon</th>
+                                            <th class="text-center">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <?php
+                                        $no = 1;
+                                        $klas = $_GET['kls'];
+                                        if ($klas == "semua") {
+                                            $sql = mysqli_query($conn, "select * from siswa");
+                                        } else {
+                                            $sql = mysqli_query($conn, "select * from siswa where idk='$_GET[kls]'");
+                                        }
+
+                                        while ($rs = mysqli_fetch_array($sql)) {
+                                            $sqlw = mysqli_query($conn, "select * from kelas where idk='$rs[idk]'");
+                                            $rsw = mysqli_fetch_array($sqlw);
+                                            $sqlb = mysqli_query($conn, "select * from sekolah where id='$rsw[id]'");
+                                            $rsb = mysqli_fetch_array($sqlb);
+
+                                            if ($_SESSION['level'] == "admin_guru") {
+
+                                                if ($rsb['id'] == $_SESSION['id']) {
+                                        ?> <tr class="odd gradeX">
+                                                        <td><?php echo "$rs[nis]";  ?></td>
+                                                        <td><?php echo "$rs[nama]";  ?></td>
+                                                        <?php
+                                                        if ($rs['jk'] == "L") {
+                                                        ?>
+                                                            <td class="text-center">Laki - Laki</td>
+                                                        <?php
+                                                        } else {
+                                                        ?>
+                                                            <td class="text-center">Perempuan</td>
+                                                        <?php
+                                                        }
+                                                        ?>
+                                                        <td><?php echo "$rs[idk]";  ?></td>
+                                                        <td><?php echo "$rs[tlp]";  ?></td>
+
+                                                        <td class="text-center">
+                                                            <a href="./././media.php?module=input_siswa&act=edit&ids=<?php echo $rs['ids'] ?>">
+                                                                <button type="button" class="btn btn-info">Edit
+                                                                </button> <a href="././module/simpan.php?act=hapus&ids=<?php echo $rs['ids'] ?>">
+                                                                    <button type="button" class="btn btn-danger">Hapus</button></a>
+                                                        </td>
+
+                                                    </tr>
+                                                <?php
+                                                }
+                                            } else {
+                                                ?>
+                                                <tr class="odd gradeX">
+                                                    <td><?php echo "$rs[nis]";  ?></td>
+                                                    <td><?php echo "$rs[nama]";  ?></td>
+                                                    <?php
+                                                    if ($rs['jk'] == "L") {
+                                                    ?>
+                                                        <td class="text-center">Laki - Laki</td>
+                                                    <?php
+                                                    } else {
+                                                    ?>
+                                                        <td class="text-center">Perempuan</td>
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                    <td><?php
+                                                        $namakelas = mysqli_query($conn, "SELECT * FROM kelas WHERE idk='$rs[idk]'");
+                                                        $nama_kelas = mysqli_fetch_array($namakelas);
+                                                        echo "$nama_kelas[nama]";  ?></td>
+                                                    <td><?php echo "$rs[tlp]";  ?></td>
+
+                                                    <td class="text-center">
+
+                                                        <a href="./././media.php?module=detail_siswa&act=details&ids=<?php echo $rs['ids'] ?>">
+                                                            <button type="button" class="btn btn-light">Details</button> </a>
+
+                                                        <a href="./././media.php?module=input_siswa&act=edit&ids=<?php echo $rs['ids'] ?>">
+                                                            <button type="button" class="btn btn-outline-primar">Edit</button> </a>
+
+
+                                                        <a href="././module/simpan.php?act=hapus&ids=<?php echo $rs['ids'] ?>">
+                                                            <button type="button" class="btn btn-outline-danger">Hapus</button></a>
+
+                                                    </td>
+                                                </tr>
+                                        <?php
+                                            }
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
